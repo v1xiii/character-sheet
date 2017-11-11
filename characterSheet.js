9aa2606b993ab16605d33e0a2237ab0aa143da79
Lee Scholl <v1xiii@users.noreply.github.com>
2017-11-10 21:26:12 -0600

    random number generator
    generator online

--- characterSheet.js
+++ characterSheet.js
@@ -1,3 +1,6 @@
 function statGenerator() {
-
+	var fields = document.getElementsByClassName('width_2rem');
+	for (i = 0; i < fields.length; i++) {
+	    fields[i].value = Math.floor(Math.random()*100)+1;
+	}
 }
